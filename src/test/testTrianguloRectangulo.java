a990f5b4b321dec38dc4f1458b0bddf111a9cb9f
ELROKA02 <samuelstonecaballero@gmail.com>
2025-05-20 17:02:01 +0200

    Examen realizado

---
+++ src/test/testTrianguloRectangulo.java
@@ -0,0 +1,69 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testTrianguloRectangulo {
+
+	@Test
+	void testTrianguloRectanguloIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTrianguloRectangulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHipotenusa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPerimetro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
